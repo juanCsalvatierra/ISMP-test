ba3aa1e5ea4a46aec27f00b0431776cfc29fd58e
juanCsalvatierra <salvatierrajuan1951@gmail.com>
2026-04-11 17:31:36 -0300

    agregando zoom especifico

--- app/components/scene/SceneCanvas.tsx
+++ app/components/scene/SceneCanvas.tsx
@@ -42,7 +42,7 @@ export function SceneCanvas({
       {scannerIndex ? <MeshScanner json={json} index={scannerIndex} /> : null}
       {showGrid ? <gridHelper args={[20, 20]} /> : null}
 
-      <OrbitControls target={[0, 1.7, 0]} />
+      <OrbitControls target={[0, 1.7, 0]} zoomToCursor />
 
       <InteractiveScene json={json}>{children}</InteractiveScene>
 
